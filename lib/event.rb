5cd93c819227c9fb55ff4ed27165d0feaf1207b6
Sage Skaff <skaffsage@gmail.com>
2022-04-26 09:04:31 -0700

    Add food_trucks_that_sell

--- lib/event.rb
+++ lib/event.rb
@@ -13,4 +13,10 @@ class Event
   def food_truck_names
     food_trucks.map { |food_truck| food_truck.name }
   end
+
+  def food_trucks_that_sell(item)
+    food_trucks.map do |food_truck|
+       food_truck if food_truck.check_stock(item) > 0 
+     end.compact
+  end
 end
