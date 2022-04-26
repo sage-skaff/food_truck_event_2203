115f9dd86c0b30b1ed2cb07f7f837c8d148b1f82
Sage Skaff <skaffsage@gmail.com>
2022-04-26 10:38:59 -0700

    Refactor

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -13,8 +13,8 @@ RSpec.describe Item do
     end
 
     it "has readable attributes" do
-      expect(@item1.name).to eq('Peach Pie (Slice)')
-      expect(@item1.price).to eq(3.75)
+      expect(@item2.name).to eq('Apple Pie (Slice)')
+      expect(@item2.price).to eq(2.50)
     end
   end
 end
